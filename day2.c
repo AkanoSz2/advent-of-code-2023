a3f4fbd44f32672a909bb1021ad11a5fade012ec
Sleepy Ako <130492458+AkanoSz2@users.noreply.github.com>
2025-09-20 09:00:52 +0300

    Add all day files after a small issue

---
+++ day2.c
@@ -0,0 +1,74 @@
+ #include <ctype.h>
+ #include <stdio.h>
+ #include <string.h>
+
+ void rendercubes(char line[256], int *gameSum) {
+     int colorPower = 1;
+     int maxRed = 12, maxGreen = 13, maxBlue = 14;
+     int red = 0, green = 0, blue = 0, ID = 0, current_value = 0;
+     int max_red = 0, max_green = 0, max_blue = 0;
+     int i;
+
+     for (i = 5; i <= strlen(line); i++) {
+         if (isdigit(line[i])) {
+             current_value = current_value * 10 + (line[i] - '0');
+         } else {
+             if (line[i] == ':' || line[i] == ';') {
+                 ID = (line[i] == ':') ? current_value : ID;
+                 if (line[i] == ';') {
+                     if (red > max_red) max_red = red;
+                     if (green > max_green) max_green = green;
+                     if (blue > max_blue) max_blue = blue;
+                     red = green = blue = 0;
+                 }
+                 current_value = 0;
+             }
+             if (line[i] == 'r') {
+                 red += current_value;
+                 current_value = 0;
+             }
+             if (line[i] == 'g') {
+                 green += current_value;
+                 current_value = 0;
+             }
+             if (line[i] == 'b') {
+                 blue += current_value;
+                 current_value = 0;
+             }
+         }
+     }
+
+     if (red > max_red) max_red = red;
+     if (green > max_green) max_green = green;
+     if (blue > max_blue) max_blue = blue;
+
+     // PART 1
+     // if (max_red <= maxRed && max_green <= maxGreen && max_blue <= maxBlue) {
+     //     *gameSum += ID;
+     // }
+
+
+     // PART 2
+     printf("Game %d: Red %d, Blue %d, Green %d\n", ID, max_red, max_blue, max_green);
+
+     colorPower = max_blue * max_green * max_red;
+     printf("Power %d\n", colorPower);
+     *gameSum += colorPower;
+ }
+
+
+ int main(void) {
+     char line[256];
+     int gameSum = 0;
+     FILE *file = fopen("C:\\Users\\Sleepy Ako\\CLionProjects\\untitled6\\input.txt", "r");
+     if (file == NULL) {
+         printf("Error opening file!\n");
+         return 1;
+     }
+     while (fgets(line, sizeof(line), file)) {
+         rendercubes(line, &gameSum);
+     }
+     printf("%d", gameSum);
+     fclose(file);
+     return 0;
+ }
---
+++ day3.c
@@ -0,0 +1,111 @@
+ #include <ctype.h>
+ #include <stdio.h>
+ #include <string.h>
+ #include <stdlib.h>
+
+ typedef struct {
+     int row, col;
+     int numbers[2];
+     int count;
+ } Gear;
+
+ Gear* findGear(Gear *gears, int *gearCount, int row, int col) {
+     for (int i = 0; i < *gearCount; i++) {
+         if (gears[i].row == row && gears[i].col == col) {
+             return &gears[i];
+         }
+     }
+     gears[*gearCount].row = row;
+     gears[*gearCount].col = col;
+     gears[*gearCount].count = 0;
+     (*gearCount)++;
+     return &gears[*gearCount - 1];
+ }
+
+ void missingGear(char matrix[200][200], int rows, int columns, int *engineSum) {
+     *engineSum = 0;
+     Gear gears[10000];
+     int gearCount = 0;
+
+     for (int i = 0; i < rows; i++) {
+         for (int j = 0; j < columns; j++) {
+             if (isdigit(matrix[i][j])) {
+                 int num = 0;
+                 int start_j = j;
+                 int len = 0;
+                 while (j < columns && isdigit(matrix[i][j])) {
+                     num = num * 10 + (matrix[i][j] - '0');
+                     len++;
+                     j++;
+                 }
+                 // Check the perimeter of the number for '*' symbols
+                 for (int jj = start_j; jj < start_j + len; jj++) {
+                     for (int di = -1; di <= 1; di++) {
+                         for (int dj = -1; dj <= 1; dj++) {
+                             if (di == 0 && dj == 0) continue;
+                             int ni = i + di;
+                             int nj = jj + dj;
+                             if (ni >= 0 && ni < rows && nj >= 0 && nj < columns && matrix[ni][nj] == '*') {
+                                 Gear *gear = findGear(gears, &gearCount, ni, nj);
+                                 if (gear->count < 2) {
+                                     int already_added = 0;
+                                     for (int k = 0; k < gear->count; k++) {
+                                         if (gear->numbers[k] == num) {
+                                             already_added = 1;
+                                             break;
+                                         }
+                                     }
+                                     if (!already_added) {
+                                         gear->numbers[gear->count] = num;
+                                         gear->count++;
+                                     }
+                                 }
+                             }
+                         }
+                     }
+                 }
+                 j--;
+             }
+         }
+     }
+
+     for (int i = 0; i < gearCount; i++) {
+         if (gears[i].count == 2) {
+             *engineSum += gears[i].numbers[0] * gears[i].numbers[1];
+         }
+     }
+ }
+
+ int main(void) {
+     char line[256];
+     int engineSum = 0;
+     char matrix[200][200];
+     int rows = 0, columns = 0;
+
+     FILE *file = fopen("C:\\Users\\Sleepy Ako\\CLionProjects\\untitled6\\input.txt", "r");
+     if (file == NULL) {
+         printf("Error opening file!\n");
+         return 1;
+     }
+
+     while (fgets(line, sizeof(line), file)) {
+         int len = strlen(line);
+         if (line[len - 1] == '\n') {
+             line[len - 1] = '\0';
+             len--;
+         }
+         columns = len;
+
+         for (int i = 0; i < len; i++) {
+             matrix[rows][i] = line[i];
+         }
+         rows++;
+     }
+
+     fclose(file);
+
+     missingGear(matrix, rows, columns, &engineSum);
+
+     printf("Engine Sum: %d\n", engineSum);
+     return 0;
+ }
